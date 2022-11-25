d1608d8587a6e15b4a9946a97f7e1577a2897cdb
RooGhz720 <rooghz720@gmail.com>
2022-11-26 01:32:41 +0700

    add support Sweet/tin

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Sleepy Kernel for Redmi Note 10 Pro & Pro Max by @ itsshashanksp
+kernel.string=Aghisna Kernel Redmi Note 10 Pro
 do.devicecheck=1
 do.modules=0
 do.systemless=1
