f7c25771cd47f038c619b5212e814271e179aad3
RooGhz720 <RooGhz720@gmail.com>
2022-12-24 02:14:16 +0700

    hyper

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Aghisna Kernel Redmi Note 10 Pro
+kernel.string=Hyperion kernel By aghisna Redmi Note 10 Pro
 do.devicecheck=1
 do.modules=0
 do.systemless=1
